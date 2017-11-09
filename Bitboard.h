c51c0c9a8cd846d62d816e5dcefbc371f34feb6e
Loïc MARIE <loic.marie54@gmail.com>
2017-11-09 14:27:53 +0100

    Added cell getters with mask, bit position and cartesian coordinates

--- Bitboard.h
+++ Bitboard.h
@@ -4,9 +4,17 @@
 #include <stdint.h>
 
 struct Bitboard {
+    int w, h;
     uint64_t state;
-    inline Bitboard() : state(0x0) {}
-    inline Bitboard(int value) : state(value) {};
+
+    inline Bitboard() : w(8), h(8), state(0x0) {}
+    inline Bitboard(int value) : w(8), h(8), state(value) {}
+    inline Bitboard(int w, int h) : w(w), h(h), state(0x0) {}
+    inline Bitboard(int value, int w, int h) : w(w), h(h), state(value) {}
+
+    inline uint64_t get_from_mask(uint64_t mask) { return state & mask; }
+    inline uint64_t get_from_bitpos(int bitpos) { return get_from_mask(UINT64_C(1) << bitpos); }
+    inline uint64_t get_from_cartesian(int x, int y) { return get_from_bitpos(y * w + x); }
 };
 
 #endif // DEF_BITBOARD
--- BitboardTest.cpp
+++ BitboardTest.cpp
@@ -1,14 +1,49 @@
+#include <iostream>
 #include <assert.h>
+#include <cmath>
 #include "Bitboard.h"
 
 void testInit() {
     Bitboard bb = Bitboard();
     assert(bb.state == 0);
+    assert(bb.w == 8);
+    assert(bb.h == 8);
 
     bb = Bitboard(1);
     assert(bb.state == 1);
+    assert(bb.w == 8);
+    assert(bb.h == 8);
+
+    bb = Bitboard(6, 6);
+    assert(bb.state == 0);
+    assert(bb.w == 6);
+    assert(bb.h == 6);
+
+    bb = Bitboard(1, 6, 6);
+    assert(bb.state == 1);
+    assert(bb.w == 6);
+    assert(bb.h == 6);
+}
+
+void testPosOperations() {
+    // Get cell on first line of board
+    uint64_t state = 0x10;
+    unsigned int bitpos = log2(state);
+    Bitboard bb = Bitboard(state);
+    assert(bb.get_from_mask(UINT64_C(1) << bitpos) == state);
+    assert(bb.get_from_bitpos(bitpos) == state);
+    assert(bb.get_from_cartesian(bitpos%bb.w, (int)(bitpos/bb.w)) == state);
+
+    // Get cell on a line of board to test cartesian coordinates
+    state = 0x2000;
+    bitpos = log2(state);
+    bb = Bitboard(state);
+    assert(bb.get_from_mask(UINT64_C(1) << bitpos) == state);
+    assert(bb.get_from_bitpos(bitpos) == state);
+    assert(bb.get_from_cartesian(bitpos%bb.w, (int)(bitpos/bb.w)) == state);
 }
 
 int main() {
     testInit();
+    testPosOperations();
 }
